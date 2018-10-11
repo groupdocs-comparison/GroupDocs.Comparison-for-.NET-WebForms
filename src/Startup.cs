c3e984f500ef1afbc46940ab743d490a73a33588
Pavel-Teplitsky <pavel.teplitsky@groupdocs.com>
2018-10-11 10:15:59 +0300

    Added GroupDocs.Comparison example

---
+++ src/Startup.cs
@@ -0,0 +1,10 @@
+﻿using Owin;
+
+namespace GroupDocs.Comparison.WebForms
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+           
+        }
+    }
+}
